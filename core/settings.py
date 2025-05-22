e5416a48700d759c1a5354319045dd3a7bae1a41
Nagham <94856051+Nagham243@users.noreply.github.com>
2025-05-22 22:30:14 +0300

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -173,8 +173,20 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
+    "https://warehouse-frontend-production.up.railway.app",
 ]
 CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken', 
+    'x-requested-with',
+]
 
 SESSION_COOKIE_AGE = 86400  # 24 hours in seconds
 SESSION_COOKIE_HTTPONLY = True  # Prevents JavaScript access
